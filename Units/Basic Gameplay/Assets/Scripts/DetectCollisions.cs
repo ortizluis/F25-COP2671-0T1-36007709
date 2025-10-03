b9a47c20b6de94a86576d19aac8b5ccbbfa213b9
Luis Ortiz <36007709@sfcollege.edu>
2025-10-02 20:00:03 -0400

    Prototype Projects

---
+++ Units/Basic Gameplay/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
